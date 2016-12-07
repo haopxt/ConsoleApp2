8bc477b02c40fb7bcf7e4cc26cf7266c9bc41085
haopxt <haopxt@DESKTOP-9UK98C7>
2016-12-07 12:10:06 +0800

    添加项目文件。

---
+++ src/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string curMach = Environment.MachineName;
+            Console.Write(curMach);
+            Console.ReadKey();
+        }
+    }
+}
